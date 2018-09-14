68458404563feab2dffb5366827e12ec848c6a93
hemingway <snowman@gabriels-imac.fios-router.home>
2018-09-13 23:16:54 -0400

    Initial Commit

---
+++ Ballz1Tests/Ballz1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Ballz1Tests.swift
+//  Ballz1Tests
+//
+//  Created by Gabriel Busto on 9/13/18.
+//  Copyright © 2018 Self. All rights reserved.
+//
+
+import XCTest
+@testable import Ballz1
+
+class Ballz1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
