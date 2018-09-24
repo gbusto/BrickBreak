e95d63fe052e2b3fb99ae99aec679a886f2824b5
Gabriel Busto <snowman@Gabriels-iMac.fios-router.home>
2018-09-23 21:10:45 -0400

    Removed physics lines from game.

--- Ballz1/GameViewController.swift
+++ Ballz1/GameViewController.swift
@@ -23,7 +23,6 @@ class GameViewController: UIViewController {
             view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
-            view.showsPhysics = true
             view.showsFPS = true
             view.showsNodeCount = true
         }
