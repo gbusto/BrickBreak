10186ec8c2cbf7154a93a1269fd65234165663a1
Gabriel Busto <gbusto.gator@gmail.com>
2018-10-17 20:41:02 -0400

    Removed unnecessary print statements

--- Ballz1/Models/ContinousGameModel.swift
+++ Ballz1/Models/ContinousGameModel.swift
@@ -266,7 +266,6 @@ class ContinuousGameModel {
         
         // Wait for the ball manager to finish
         if ballManager!.isDone() {
-            print("BALL MANAGER IS DONE")
             // Increment state from MID_TURN to TURN_OVER
             incrementState()
             
@@ -341,7 +340,6 @@ class ContinuousGameModel {
     public func animationsDone() -> Bool {
         if itemGenerator!.isReady() {
             // Change state from WAITING to READY
-            print("MODEL IS READY")
             incrementState()
             return true
         }
