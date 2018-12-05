95d04e577eec46d814ecd126ccfc4797aee49495
Gabriel Busto <gbusto.gator@gmail.com>
2018-12-04 22:53:53 -0500

    Added Game Center integration code.
    It's impossible to know whether or not scores are properly being
    submitted, but the code seems right. The leader board just isn't
    live and it won't go live until it's submitted for approval.

--- Ballz1/Controllers/GameMenuController.swift
+++ Ballz1/Controllers/GameMenuController.swift
@@ -9,12 +9,84 @@
 //
 
 import UIKit
+import GameKit
 import SpriteKit
 import GameplayKit
 
-class GameMenuController: UIViewController {
+class GameMenuController: UIViewController, GKGameCenterControllerDelegate {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet var gameCenterButton: UIButton!
+    
+    /* Variables */
+    var gcEnabled = Bool() // Check if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Check the default leaderboardID
+    
+    // IMPORTANT: replace the red string below with your own Leaderboard ID (the one you've set in iTunes Connect)
+    let LEADERBOARD_ID = "xyz.ashgames.brickbreak"
+    
+    struct PersistentData: Codable {
+        var highScore: Int
+        var showedTutorials: Bool
+        
+        // This serves as the authoritative list of properties that must be included when instances of a codable type are encoded or decoded
+        // Read Apple's documentation on CodingKey protocol and Codable
+        enum CodingKeys: String, CodingKey {
+            case highScore
+            case showedTutorials
+        }
+    }
+    
+    func loadHighScore() -> Int {
+        do {
+            let pData = try Data(contentsOf: ContinuousGameModel.PersistentDataURL)
+            let persistentData = try PropertyListDecoder().decode(PersistentData.self, from: pData)
+        
+            print("Loaded persistent data")
+            return persistentData.highScore
+        }
+        catch {
+            print("Error decoding persistent game state: \(error)")
+            return 0
+        }
+    }
+    
+    // MARK: Gamecenter delegate protocol
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        // Not being used at this time
+    }
+    
+    func authenticatePlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.local
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if ((ViewController) != nil) {
+                // 1. Show login if player is not logged in
+                self.present(ViewController!, animated: true, completion: nil)
+            }
+            else if (localPlayer.isAuthenticated) {
+                // 2. Player is already authenticated and logged in; load game center
+                self.gcEnabled = true
+                
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifier, error) in
+                    if error != nil {
+                        print("ERROR: \(error!)")
+                    }
+                    else {
+                        // I'm assuming the app uses the default leaderboard until one is created for the game
+                        // When the first score is reported to a leaderboard, that board is now the default one
+                        self.gcDefaultLeaderBoard = leaderboardIdentifier!
+                    }
+                })
+            }
+            else {
+                // 3. Game center is not enabled on the users device
+                self.gcEnabled = false
+                print("Local player could not be authenticated!")
+                print("Error: \(error!)")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +96,39 @@ class GameMenuController: UIViewController {
             scene.scaleMode = .aspectFill
             
             playButton.imageView?.contentMode = .scaleAspectFit
+            gameCenterButton.imageView?.contentMode = .scaleAspectFit
             
             view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
+            
+            // Perform game center authentication
+            authenticatePlayer()
+            
+            // Try to get the player's high score from storage
+            let highScore = loadHighScore()
+            
+            // Report the game score to the game center
+            let gkscore = GKScore(leaderboardIdentifier: LEADERBOARD_ID)
+            gkscore.value = Int64(highScore)
+            GKScore.report([gkscore]) { (error) in
+                if error != nil {
+                    print("Error reporting score: \(error!)")
+                }
+                else {
+                    print("Successfully reported game score \(gkscore.value)")
+                }
+            }
+        }
+    }
+    
+    @IBAction func showGameCenter(_ sender: Any) {
+        if self.gcEnabled {
+            let gameCenterController = GKGameCenterViewController()
+            gameCenterController.gameCenterDelegate = self
+            gameCenterController.viewState = .leaderboards
+            gameCenterController.leaderboardIdentifier = LEADERBOARD_ID
+            self.present(gameCenterController, animated: true, completion: nil)
         }
     }
     
