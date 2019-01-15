0895c1b30e758e4e2ea795b44b3665cfa41f66ad
Anthony B <gbusto.gator@gmail.com>
2019-01-14 22:22:30 -0500

    Started adding code to the levels game controller

--- Ballz1/Controllers/LevelsGameController.swift
+++ Ballz1/Controllers/LevelsGameController.swift
@@ -13,7 +13,18 @@ import GoogleMobileAds
 class LevelsGameController: UIViewController,
                             GADBannerViewDelegate,
                             GADInterstitialDelegate {
-
+    
+    @IBOutlet weak var levelCount: UILabel!
+    @IBOutlet weak var levelScore: UILabel!
+    
+    @IBOutlet var pauseMenuView: UIView!
+    @IBOutlet weak var resumeButton: UIButton!
+    @IBOutlet weak var gameMenuButton: UIButton!
+    
+    @IBOutlet weak var bannerAdView: UIView!
+    
+    private var scene: SKScene?
+    
     override func viewDidAppear(_ animated: Bool) {
         // Load the stuff for ads
         print("Levels view appeared!")
@@ -28,12 +39,31 @@ class LevelsGameController: UIViewController,
         
         if let view = self.view as! SKView? {
             let scene = LevelsGameScene(size: view.bounds.size)
+            self.scene = scene
             
             scene.scaleMode = .aspectFill
             
             view.presentScene(scene)
             view.ignoresSiblingOrder = true
         }
+    }
+    
+    @IBAction func statusBarTapped(_ sender: UITapGestureRecognizer) {
+        let scene = self.scene as! LevelsGameScene
+        if let view = self.view as! SKView? {
+            scene.isPaused = true
+            view.isPaused = true
+            scene.showPauseScreen(pauseView: pauseMenuView)
+        }
+    }
+    
+    @IBAction func resumeButtonPressed(_ sender: Any) {
+        let scene = self.scene as! LevelsGameScene
+        scene.resumeGame()
+    }
+    
+    @IBAction func gameMenuButtonPressed(_ sender: Any) {
+        print("Return to game menu")
     }
     
     // MARK: View override functions
